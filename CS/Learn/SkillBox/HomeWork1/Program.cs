4b97d44ec64721f267fb6199c70a95678bcec520
Broooooo-dev <94326832+Broooooo-dev@users.noreply.github.com>
2022-09-28 23:38:17 +0200

    first commit

---
+++ CS/Learn/SkillBox/HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TS/Learn/01_Type/type.ts
@@ -0,0 +1,11 @@
+
+
+const varBoolean: boolean = true;
+
+const varInt: number = 5;
+
+const varString: string = 'Hello world';
+
+const ArrNumber: number[] = [1, 2, 3];
+
+const ArrTyple: [string, number] = ['Hello', 5];
